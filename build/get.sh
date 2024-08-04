75bba0f99083876753f7b4691b1f95e455013610
RollinJay <Justoptheg.o.a.t@gmail.com>
2024-08-03 19:30:59 -0700

    Update Build Scripts

--- build/get.sh
+++ build/get.sh
@@ -9,7 +9,7 @@ if [ ! -f ./go.mod ]; then
 	go mod init github.com/ikemen-engine/Ikemen-GO/src
 	echo ""
 fi
-
+go get github.com/assemblaj/ggpo
 go get -v -u github.com/samhocevar/beep
 go get -v -u github.com/flopp/go-findfont
 go get -v -u github.com/go-gl/gl/v2.1/gl
@@ -17,5 +17,6 @@ go get -v -u github.com/go-gl/glfw/v3.3/glfw
 go get -v -u github.com/ikemen-engine/glfont
 go get -v -u github.com/sqweek/dialog
 go get -v -u github.com/yuin/gopher-lua
-
+go get -v -u github.com/lukegb/dds
+go get -v -u github.com/qmuntal/gltf
 go get -v -u github.com/golang/freetype
